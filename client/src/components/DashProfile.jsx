7398a0db1594aae60247edabdf6acc8fe40942eb
Prasad <prasad.gujar2004@gmail.com>
2024-01-27 05:11:22 +0530

    add create a post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,10 +8,11 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure,signOutStart,signOutSuccess,signOutFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import {Link} from 'react-router-dom';
 // import e from 'express';
 
 export default function DashProfile() {
-    const {currentUser,error} = useSelector(state=>state.user)
+    const {currentUser,error,loading} = useSelector(state=>state.user)
     const [imageFile,setImageFile] = useState(null)
     const [imageFileURL,setImageFileURL] = useState(null);
     const [imageFileUploadProgress,setImageFileUploadProgress] = useState(null);
@@ -195,9 +196,17 @@ export default function DashProfile() {
           <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleUpdate}/>
           <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleUpdate}/>
           <TextInput type='password' id='password' placeholder='password' onChange={handleUpdate}/>
-          <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            Update
+          <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading||imageFileUploading}>
+            {loading ? 'Loading...':'Update'}
+          
           </Button>
+          {
+            currentUser.isAdmin && (
+              <Link to='/create-post'>
+                <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>Create a post</Button>
+              </Link>
+            )
+          }
         </form>
         
         <div className="text-red-500 flex justify-between mt-5">
