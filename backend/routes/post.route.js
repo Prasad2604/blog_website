70f6333ac6c50b2e2cab561912481f7f6615032b
Prasad <prasad.gujar2004@gmail.com>
2024-02-06 21:01:00 +0530

    create getposts api route

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {create} = require('../controllers/post.controller')
+const {create, getposts} = require('../controllers/post.controller')
 const verifyToken = require('../middlewares/verifyUser')
 
 router.post('/create',verifyToken,create)
+router.route('/getposts').get(getposts)
 
 module.exports = router
