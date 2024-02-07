579678dcbb1772dff07704f6a5497586354a66e8
Prasad <prasad.gujar2004@gmail.com>
2024-02-07 12:27:59 +0530

    complete home page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
+import PostCard from '../components/PostCard';
 
 export default function Home() {
   const [posts,setPosts] = useState([]);
@@ -7,23 +8,13 @@ export default function Home() {
   // console.log(posts);
 
   useEffect(()=>{
-    const fetchPosts = async ()=>{
-      try {
-        const res = fetch(`/api/post/getposts`)
-        const data = (await res).json();
-        if(!res.ok){
-          console.log(data.message);
-          return;
-        }
-        if(res.ok){
-          setPosts(data.posts);
-        }
-      } catch (error) {
-          console.log(error.message);
-      }
-    }
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
     fetchPosts();
-  },[]);
+  }, []);
 
   return (
     <div>
@@ -34,24 +25,25 @@ export default function Home() {
         software engineering, and programming languages.</p>
       <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all posts</Link>
     </div>
-    <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-      {
-        posts && posts.length>0 && 
-        <div className="">
 
-          <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
-          {/* <div className="">
-            {posts.map((post)=>(
-              
-            )
-
-            )}
-          </div> */}
-        </div>
-        
-      }
+    <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-    
-  )
+  );
 }
